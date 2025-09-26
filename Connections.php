d9c54ea7d86e7eb51083aa5a7ad709ab5d59e87f
Andy <lolniandy@gmail.com>
2025-09-26 22:48:02 +0800

    Test

--- Connections.php
+++ Connections.php
@@ -7,10 +7,9 @@
     try {
         $dsn = "mysql:host=$host;dbname=$dbname;charset=utf8mb4";
         $pdo = new PDO($dsn, $username, $password);
-
-        // Set PDO error mode to exception
         $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     } catch (PDOException $e) {
-        die("Connection failed: " . $e->getMessage());
+        // stop execution on failure
+        die("Connection failed.");
     }
 ?>
